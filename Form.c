6800ea96a9721357b6b06c2ea391ce3f666e7085
singhmission <113301245+singhmission@users.noreply.github.com>
2022-09-11 21:29:33 +0530

    add initial Anand website file

---
+++ Form.c
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>SCC COUNSTABLE FORM</title>
+</head>
+<body>
+<form>
+<table cellspacing="6" cellpadding="5">
+<tr>
+<td><label>FIRST NAME:</lable></td>
+<td><input type="text"></td>
+<td><label>LAST NAME:</lable></td>
+<td><input type="text"></td>
+</tr>
+</table>
+</form>
+</body>
+</html>
